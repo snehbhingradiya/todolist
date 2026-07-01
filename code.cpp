b605470e8e1f19473856fc4afbdde6c01a7abd3a
snehbhingradiya <snehbhingradiya81@gmail.com>
2026-07-01 18:40:57 +0530

    helloe

--- code.cpp
+++ code.cpp
@@ -2,12 +2,12 @@
 using namespace std;
 int main() {
    int n=4;
-   int num=0;
+   
     for(int i=0;i<n;i++){
-
-      for(int j=0;j<i+1;j++){
+int num=0;
+      for(int j=i+1;j>0;j--){
     num++;
-        cout<<num<< " ";
+        cout<<j;
       }
 
     cout<<endl;
