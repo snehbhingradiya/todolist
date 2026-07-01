0b6e51a476f0a9c36830890178c401a058a82c77
snehbhingradiya <snehbhingradiya81@gmail.com>
2026-07-01 21:09:02 +0530

    hardddestest so farrr

--- code.cpp
+++ code.cpp
@@ -3,44 +3,60 @@ using namespace std;
 int main() {
     int n = 4;
     for(int i = 0; i < n; i++){
-        
+
+
+
+        for(int j=0;j<i+1;j++){
+            cout<<"*";
+        }
+     
         
         for(int j=0;j<n-i-1;j++){
             cout<<" ";
-        }         
-                cout<<"*";
-
- if(i!=0){
-    for(int j=0;j<2*i-1;j++){
-        cout<< " ";
-    }
-        cout<<"*";
-            }
-            cout<<endl;
         }
 
 
+        for(int j=n-i-1;j>0;j--){
+            cout << " ";
+        }
 
-        for(int i=0;i<n-1;i++){
-            
-           for (int j=0;j<i+1;j++){
-                cout << " ";
-            }
+
+        for (int j=i+1;j>0;j--){
             cout<<"*";
+        }
+        cout<<endl;
 
-           if(i!=n-2){
-            for(int j=0;j<2*(n-i)-5;j++){
-                cout<<" ";
+    } 
+       
+//hellow guys//
+     for(int i = 0; i < n; i++){
 
-            }
+
+
+        for(int j=0;j<n-i;j++){
             cout<<"*";
-           }
- 
-
+        }
+     
         
-
-cout<<endl;
+        for(int j=0;j<i;j++){
+            cout<<" ";
         }
+
+
+        for(int j=i;j>0;j--){
+            cout << " ";
+        }
+
+
+        for (int j=n-i;j>0;j--){
+            cout<<"*";
+        }
+cout<<endl;
+    } 
+
+
+
+
 
      return 0;
     }
