97b0a91d3461cd315031fc72b3e16367150854a8
snehbhingradiya <snehbhingradiya81@gmail.com>
2026-07-01 19:32:27 +0530

    Refactor pyramid printing logic and remove unused variables

--- code.cpp
+++ code.cpp
@@ -1,19 +1,26 @@
 #include <iostream>
 using namespace std;
 int main() {
-   int n=4;
-   
+   int n=4,num=1;
     for(int i=0;i<n;i++){
-int num=0;
-      for(int j=i+1;j>0;j--){
-    num++;
-        cout<<j;
-      }
 
-    cout<<endl;
-    }
-    return 0;
-}
+
+           for(int j=0;j<i;j++){
+        cout << " ";
+           }
+
+          for (int j=0;j<n-i;j++){
+            cout<<(i+1);
+          
+        }
+         cout<<endl;
+      }
+     
+      return 0;
+      }
+    
+    
+
 
    
       
