7bc918862999f7d5f191bb60aedd9ca8250d4bcb
snehbhingradiya <snehbhingradiya81@gmail.com>
2026-07-01 19:32:31 +0530

    reegr

--- code.cpp
+++ code.cpp
@@ -1,42 +1,25 @@
 #include <iostream>
 using namespace std;
 int main() {
-   int n=4,num=1;
-    for(int i=0;i<n;i++){
+    int n = 4;
+    for(int i = 0; i < n; i++){
+        char ch = '@';
 
-
-           for(int j=0;j<i;j++){
-        cout << " ";
-           }
-
-          for (int j=0;j<n-i;j++){
-            cout<<(i+1);
-          
+        for(int j = 0; j < n-i-1; j++){
+            cout << " ";
         }
-         cout<<endl;
-      }
-     
-      return 0;
-      }
-    
-    
 
+        for(int j = 1; j <= i+1; j++){
+            ch = ch + 1;
+            cout << ch;
+        }
 
-   
-      
+        for(int j = i; j >= 1; j--){
+            ch = ch - 1;
+            cout << ch;
+        }
 
-
-
- 
-     
-   
-
-   
-    
-
-   
-   
-
-   
-
-   
+        cout << endl;
+    }
+    return 0;
+}
